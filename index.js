af796ce0cb8f3d972357686fbf747f9582e62282
Ankit Bhawnani <ankit@jalantechnologies.com>
2022-02-09 13:25:47 +0530

    Feat: Heroku deployment ready (#3)
    * fix: added PORT
    
    * fix: added start script
    
    * fix: fixed env
    
    * chore: added app.json
    
    * feat: added workflow file
    
    * feat: added addRemote
    
    * feat: changed to deploy
    
    * feat: changed to deploy
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline
    
    * feat: added pipeline

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 
-const appConfigEnv = process.NODE_CONFIG_ENV || 'development';
+const appConfigEnv = process.env.NODE_CONFIG_ENV || 'development';
+const appHttpPort = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end(`Hello from - ${appConfigEnv}`);
+  res.writeHead(200);
+  res.end(`Hello from - ${appConfigEnv}`);
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(appHttpPort);
